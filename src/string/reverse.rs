6b114b045f7bce98fbe587e609bc668fa46db256
etctera <yilonglei790@gmail.com>
2023-08-26 22:59:26 +0800

    feat: reverse string impl with comment

---
+++ src/string/reverse.rs
@@ -0,0 +1,20 @@
+pub fn main() {}
+
+pub fn reverse(text: &str) -> String {
+    text.chars().rev().collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_simple() {
+        assert_eq!(reverse("racecar"), "racecar");
+    }
+
+    #[test]
+    fn test_sentence() {
+        assert_eq!(reverse("step on no pets"), "step on no pets");
+    }
+}
